dcddb545c61750d60ed4646f22ad0ce2dff7c600
gidskql6671 <gidskql6671@gmail.com>
2023-05-07 19:58:52 +0900

    4주차 추가

--- java_example/src/Main.java
+++ java_example/src/Main.java
@@ -1,15 +1,34 @@
 import java.util.Scanner;
 
+class Sample {
+	public int nonStaticNum;
+
+	public void changeStaticNum(int n) {
+		staticNum = n;
+	}
+
+	public void printStaticNum() {
+		System.out.println("staticNum = " + staticNum);
+	}
+
+	public static int staticNum = 0;
+}
+
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Sample sample1 = new Sample();
+		Sample sample2 = new Sample();
 
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
+		sample1.nonStaticNum = 10;
+		sample2.nonStaticNum = 20;
 
-		System.out.println(a / b);
+		System.out.println("sample1.nonStaticNum = " + sample1.nonStaticNum);
+		System.out.println("sample2.nonStaticNum = " + sample2.nonStaticNum);
 
-		System.out.println("실행되야 함");
+		sample1.changeStaticNum(10);
+
+		sample1.printStaticNum();
+		sample2.printStaticNum();
 	}
 
 }
